d390316e43da5e115c2a1c16fde7183dafa3e7b1
yeger <yeger00@gmail.com>
2020-01-21 18:09:48 -0500

    bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ class PyTest(TestCommand):
 
 setup(
     name="its",
-    version="0.0.1",
+    version="0.0.2",
     author="Avi Yeger",
     author_email="yeger00@gmail.com",
     description="An “Issue tracking system” as part of your repository",
